d2ccfeb51c06601b3f46b6167126018090af574d
roman pedro meclazcke <romanmeclazcke12345@gmail.com>
2024-06-18 16:51:48 -0300

    actualice vistas y agregue directivas routerlink

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
 import { AboutSerieComponent } from "./about-serie/about-serie.component";
 import { HomeChartersComponent } from "./home-charters/home-charters.component";
 
@@ -12,7 +12,7 @@ import { HomeChartersComponent } from "./home-charters/home-charters.component";
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, AboutSerieComponent, HomeChartersComponent]
+    imports: [RouterOutlet, AboutSerieComponent, HomeChartersComponent,RouterModule]
 })
 export class AppComponent {
   title = 'tpeSeminario';
